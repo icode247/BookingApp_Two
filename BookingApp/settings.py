2a1ef9462257aa166dc970393792b727b404ddd4
icode247 <zionekekenta@gmail.com>
2022-03-08 15:22:37 +0100

    Done with frontend

--- BookingApp/settings.py
+++ BookingApp/settings.py
@@ -37,7 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'flight'
+    'corsheaders',
+    'flight',
+    'frontend'
 ]
 
 MIDDLEWARE = [
@@ -48,14 +50,16 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
-
+CORS_ORIGIN_ALLOW_ALL = True
+ALLOWED_HOSTS = ['*']
 ROOT_URLCONF = 'BookingApp.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,6 +121,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+# Add these new lines
+STATICFILES_DIRS = (
+   Path(BASE_DIR, 'static'),
+)
+
+STATIC_ROOT = Path(BASE_DIR,'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
